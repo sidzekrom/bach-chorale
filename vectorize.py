27d9f6f00e07c91376ab21809c195b55f8866749
Sidhanth Mohanty <sidhanthmohanty@Sidhanths-MacBook-Pro-2.local>
2015-07-09 23:50:00 +0530

    First commit

---
+++ vectorize.py
@@ -0,0 +1,29 @@
+import sys
+
+a = open('chorales.lisp', 'r')
+
+#parse the input as vectors and store vectors
+
+def obtainNum(elemSt):
+    a = elemSt.split(" ")
+    return int(a[1])
+
+bookOfLists = []
+
+for i in range(130):
+    counter = 0
+    gun = a.readline()
+    if (len(gun) <= 1):
+        continue
+    else:
+        while (gun[counter:(counter+2)] != "(("):
+            counter += 1
+        tribo = gun[(counter+2):(len(gun)-4)]
+        stringArr = tribo.split("))((") #separates each vector into an element
+        lister = map(lambda x : x.split(") ("), stringArr) #each vector becomes
+        #a list of component elements so lister is a list of lists
+        lister2 = map(lambda x : map(obtainNum, x), lister)
+        bookOfLists.append(lister2)
+
+#lister2 in each iteration is the time series data of the vectors
+#bookOfLists is many time series datasets put together
