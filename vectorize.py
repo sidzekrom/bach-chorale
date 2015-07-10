a74e992c69ebaf193d3b74c6b1e1db6716c0f9b7
Praful Shankar <prflshankar@gmail.com>
2015-07-10 14:36:01 +0530

    :work: global universal dict and list and frequency of states

--- vectorize.py
+++ vectorize.py
@@ -32,7 +32,6 @@ for i in range(130):
 #lister2 in each iteration is the time series data of the vectors
 #bookOfLists is many time series datasets put together
 
-# dictionary where we reduce the given variables
 def classify(note):
     classified_note = [0,0,0,0,0]
     classified_note[0] = (note[1]-60)//4
@@ -41,11 +40,27 @@ def classify(note):
     classified_note[3] = (note[4]-12)//4
     classified_note[4] = (note[5])
     return classified_note
+#self-explanatory changing of 6 list note to 5 list classification
 
 bookOfClasses = []
+#map of classify to bookOfLists
+universal_states = []
+#list of dictionaries of bookOfClasses and its count per line
+global_sum_states = {}
+#dictionary of total counts of universal_states keys across all lines
+
 for line in bookOfLists:
+    states = {}
     rifle = []
     for note in line:
         classified_note = classify(note)
+        if str(classified_note) not in states:
+            states[str(classified_note)] = 1
+            global_sum_states[str(classified_note)] = 1
+        else:
+            states[str(classified_note)] += 1
+            global_sum_states[str(classified_note)] += 1
         rifle.append(classified_note)
     bookOfClasses.append(rifle)
+    universal_states.append(states)
+#panthi code to achieve the dicts and lists
